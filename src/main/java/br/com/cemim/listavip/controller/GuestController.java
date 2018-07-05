c01790cd279398654070a3b03e00285da3dab408
Igor Cemim <igor@cemim.com.br>
2018-07-05 01:39:30 -0300

    Adicionado service

--- src/main/java/br/com/cemim/listavip/controller/GuestController.java
+++ src/main/java/br/com/cemim/listavip/controller/GuestController.java
@@ -1,7 +1,8 @@
-package br.com.cemim.listavip;
+package br.com.cemim.listavip.controller;
 
 import java.util.List;
 
+import br.com.cemim.listavip.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,33 +11,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cemim.listavip.model.Guest;
-import br.com.cemim.listavip.repository.GuestRepository;
 
 @RestController
 public class GuestController {
 
     @Autowired
-    private GuestRepository repository;
+    private GuestService service;
 
     @RequestMapping(path = "/guests", method = RequestMethod.GET)
     public List<Guest> list() {
-        return repository.findAll();
+        return service.list();
     }
 
     @RequestMapping(path = "/guests/{id}", method = RequestMethod.GET)
     public Guest get(@PathVariable Long id) {
-        return repository.findById(id).orElse(null);
+        return service.get(id);
     }
 
     @RequestMapping(path = "/guests", method = RequestMethod.POST)
     public Guest save(@RequestBody Guest guest) {
-        repository.save(guest);
+        service.save(guest);
         return guest;
     }
 
     @RequestMapping(path = "/guests/{id}", method = RequestMethod.DELETE)
-    public void save(@PathVariable Long id) {
-        repository.deleteById(id);
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 
 }
---
+++ src/main/java/br/com/cemim/listavip/service/GuestService.java
@@ -0,0 +1,49 @@
+package br.com.cemim.listavip.service;
+
+import br.com.cemim.listavip.AppConfig;
+import br.com.cemim.listavip.model.Guest;
+import br.com.cemim.listavip.repository.GuestRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class GuestService {
+
+    private static final Logger logger = LoggerFactory.getLogger(GuestService.class);
+
+    @Autowired
+    private GuestRepository repository;
+
+    @Autowired
+    private AppConfig app;
+
+    public List<Guest> list() {
+        return repository.findAll();
+    }
+
+    public Guest get(@PathVariable Long id) {
+        Guest guest = repository.findById(id).orElse(null);
+        logger.info("Got " + guest.toString());
+        return guest;
+    }
+
+    public Guest save(@RequestBody Guest guest) {
+        repository.save(guest);
+        logger.info("Saved " + guest.toString());
+        return guest;
+    }
+
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+        logger.info("Deleted Guest of id " + id);
+    }
+
+}
